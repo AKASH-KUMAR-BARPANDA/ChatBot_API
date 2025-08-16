208b5ef5bfe5828b6c5f967b6cdfbbac06f27cc0
akashbarpanda <akbarpanda15@gmail.com>
2025-08-17 01:50:09 +0530

    Routes logic functionh

---
+++ REPOSITORY/CHAT.py
@@ -0,0 +1,10 @@
+from GROQ_SERVER import GROQ_SERVER_API
+
+
+
+def get_bot_response(user_message):
+    message = user_message.lower()
+    response_from_server = GROQ_SERVER_API.GROQ_Server_INJECTION(message)
+    return response_from_server
+
+
