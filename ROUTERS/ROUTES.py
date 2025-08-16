3affbfa1cb44b89240ce8ca7cb14f20c235b3ebf
akashbarpanda <akbarpanda15@gmail.com>
2025-08-17 01:42:27 +0530

    Routes

---
+++ ROUTERS/ROUTES.py
@@ -0,0 +1,15 @@
+
+from  fastapi import APIRouter
+from BASE_PYDANTIC import BASEMODEL
+from REPOSITORY import CHAT
+
+router = APIRouter(
+    tags = ["CHAT"],
+    prefix = '/chats'
+)
+
+
+@router.post('')
+def chat( request : BASEMODEL.ChatRequest):
+    replay = CHAT.get_bot_response(request.Message)
+    return {"replay" : replay}
