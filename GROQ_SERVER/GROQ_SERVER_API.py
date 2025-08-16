312caeca80e64342648c75484ec798087ae4e3fc
akashbarpanda <akbarpanda15@gmail.com>
2025-08-17 01:41:04 +0530

    LLM Service Provider

---
+++ GROQ_SERVER/GROQ_SERVER_API.py
@@ -0,0 +1,19 @@
+
+from groq import Groq
+client = Groq(
+        api_key="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"
+    )
+
+def GROQ_Server_INJECTION(message):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": message,
+            }
+        ],
+        model="openai/gpt-oss-20b",
+        stream=False,
+    )
+
+    return chat_completion.choices[0].message.content
